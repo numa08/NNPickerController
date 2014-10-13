7f9a5c3ef5c894da382f6f0a4634afb9a7065f9f
numa08 <n511287@gmail.com>
2014-10-13 11:59:24 +0900

    add core class

---
+++ Pod/Classes/NNPickerController.h
@@ -0,0 +1,12 @@
+//
+//  NNPickerController.h
+//
+//  Created by numanuma08 on 2014/10/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNPickerController : UIViewController
+
+@end
---
+++ Pod/Classes/NNPickerController.m
@@ -0,0 +1,14 @@
+//
+//  NNPickerController.m
+//
+//  Created by numanuma08 on 2014/10/13.
+//
+//
+
+#import "NNPickerController.h"
+
+@interface NNPickerController ()
+@end
+
+@implementation NNPickerController
+@end
