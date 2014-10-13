291360a15f30fa2b6837a21ed1f5e7c09415af2c
numa08 <n511287@gmail.com>
2014-10-13 13:49:14 +0900

    write example project

--- Example/NNPickerController/NNViewController.m
+++ Example/NNPickerController/NNViewController.m
@@ -8,22 +8,46 @@
 
 #import "NNViewController.h"
 
-@interface NNViewController ()
+#import <NNPickerController/NNPickerController.h>
 
+@interface NNViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *elementLabel;
 @end
 
 @implementation NNViewController
 
-- (void)viewDidLoad
+- (IBAction)didClickPick:(id)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSArray *girls = @[@"でじこ", @"うさだ", @"ぷちこ", @"ぴよこ", @"ミケ", @"リンナ", @"あかり", @"ゲマ", @"ほっけみりん", @"まじんがっぱ"];
+    NNPickerController *picker = [[NNPickerController alloc] init];
+    picker.cancelHandler = ^(NNPickerController *picker) {
+        [picker dismissPickerController];
+    };
+    picker.finishPickingHandler = ^(NNPickerController *picker, UITableView *tableView,NSIndexPath *indexPath){
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        self.elementLabel.text = girls[indexPath.row];
+        [picker dismissPickerController];
+    };
+    
+    [picker setNumberOfSection:^NSInteger{
+        return 1;
+    } withNumberOfRow:^NSInteger(NSInteger section) {
+        return girls.count;
+    } withCellForRowAtIndexPath:^UITableViewCell *(UITableView *tableView, NSIndexPath *indexPath) {
+        NSString *cellIdentifier = [NSString stringWithFormat:@"cell%@/%@", @(indexPath.row),@(indexPath.section)];
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+        if (cell != nil) {
+            return cell;
+        }
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+        cell.textLabel.text = girls[indexPath.row];
+        if (indexPath.row == 0) {
+            cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        }
+        return cell;
+    }];
+    [picker showPickerControllerForViewController];
 }
 
 @end
